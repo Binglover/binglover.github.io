2db835498bccd7a9b26572bd52a66a6f5a98cf68
Binglover <154568951+Binglover@users.noreply.github.com>
2026-09-24 18:05:03 -0500

    Reverted back to working version

--- script.js
+++ script.js
@@ -230,30 +230,20 @@ object, iframe {
 <body>
 <!-- ✅ SIDEBAR MENU -->
 <div class="sidebar">
+  <div class="menu-btn" data-tab="plumet">🎮 Plumet</div>
+  <div class="menu-btn" data-tab="cookie">🍪 Cookie Clicker</div>
+</div>
 
-  <div class="menu-btn" data-tab="plumet">
-    🎮 Plumet
+<!-- ✅ MAIN VIEW -->
+<div class="content">
+  <div id="tab-plumet" class="tab-view">
+      <object id="game-object" data="Plumet2.swf" type="application/x-shockwave-flash"></object>
   </div>
 
-  <div class="menu-btn" data-tab="cookie">
-    🍪 Cookie Clicker
+  <div id="tab-cookie" class="tab-view">
+      <iframe src="https://binglover.github.io/cookieclicker/index.html"></iframe>
   </div>
-
-  <div class="menu-btn" data-tab="slope">
-    🏂 Slope
-  </div>
-
-  <div class="menu-btn" data-tab="run3">
-    🚀 Run 3
-  </div>
-
-  <div class="menu-btn" data-tab="flappybird">
-    🐦 Flappy Bird
-  </div>
-
-  <div class="menu-btn" data-tab="tetris">
-    🧱 Tetris
-  </div>
+</div>
 
 <script src="https://unpkg.com/@ruffle-rs/ruffle"></script>
 
